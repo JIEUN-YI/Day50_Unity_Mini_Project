a21df496e51a4354da16db614f66ee5a1e179f22
YIJEUN <yijeun.dev@gmail.com>
2024-09-25 23:07:44 +0900

    [develop]Player_플레이어의 움직임 구현
    1. 시간이 지날수록 Hp의 감소가 발생함
    - 단, Hurdle 관련 script 파일에서 hp의 감소가 독자적으로 일어나는 것으로 PlayerController.cs파일과 연관이 없음
    - playerHp 변수를 PlayerController에서 한번에 제어하도록 수정이 필요함
    2. 2단 점프 기능 완성
    3. 슬라이드 기능 완성
    4. 장애물과 충돌 시 체력 감소 완성

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,8 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float playerHp;
+
+    [SerializeField] float jumpPower;
+    [SerializeField] int jumpCount = 0;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (jumpCount < 2)
+            {
+                jumpCount++;
+                Jump();
+            }
+            else if (jumpCount >= 2)
+            {
+                return;
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Sliding();
+            Debug.Log("Sliding");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Standing();
+            Debug.Log("Standing");
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+
+    private void Sliding()
+    {
+        // 게임오브젝트 90도 회전
+        transform.rotation = Quaternion.Euler(0, 0, 90);
+    }
+
+    private void Standing()
+    {
+        // 게임오브젝트 원상 복귀
+        transform.position = new Vector2(-4, -2.3f);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "UnderGround":
+                jumpCount = 0;
+                break;
+            case "Hurdle":
+                playerHp -= 3;
+                break;
+        }
+    }
 }
