54567e3930670631d79027d032b43ccff10a1e98
YIJEUN <yijeun.dev@gmail.com>
2024-09-28 16:42:47 +0900

    [Add]develop branch
    develop 브런치의 생성 후 확인 커밋

--- Assets/PatternPool.cs
+++ Assets/PatternPool.cs
@@ -59,6 +59,8 @@ public class PatternPool : MonoBehaviour
     /// <param name="returnPatternPool"></param>
     public void ReturnPatternPool(PatternObj pattern, List<PatternObj> returnPatternPool)
     {
+        pattern.transform.Find("Gem").gameObject.SetActive(true);
+        Debug.Log($"Gem의 활성화");
         pattern.gameObject.SetActive (false);
         pattern.transform.parent = makePoint;
         returnPatternPool.Add(pattern);
