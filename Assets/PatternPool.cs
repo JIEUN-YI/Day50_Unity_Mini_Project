1970bb5c061888a11da8fbe81380abcabdfd05a7
YIJEUN <yijeun.dev@gmail.com>
2024-09-28 18:55:15 +0900

    [commit] Pattern의 Gem 기능 제작 완성

--- Assets/PatternPool.cs
+++ Assets/PatternPool.cs
@@ -59,8 +59,6 @@ public class PatternPool : MonoBehaviour
     /// <param name="returnPatternPool"></param>
     public void ReturnPatternPool(PatternObj pattern, List<PatternObj> returnPatternPool)
     {
-        GameObject.FindGameObjectWithTag("Gem").SetActive(true);
-        Debug.Log($"{GameObject.FindGameObjectWithTag("Gem")} Gem의 활성화");
         pattern.gameObject.SetActive (false);
         pattern.transform.parent = makePoint;
         returnPatternPool.Add(pattern);
